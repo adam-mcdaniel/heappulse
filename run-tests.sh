6bece80b848dd8dd5f85c326c509ee014600ff8f
Adam McDaniel <adam.mcdaniel17@gmail.com>
2023-07-02 21:13:08 -0400

    Moved executables

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
-g++ tests/test1.cpp -o test1.exe -g
-./run.sh ./test1.exe tests/test1.in > tests/test1.out 2> tests/test1.err
+g++ tests/test1.cpp -o tests/test1.exe -g
+./run.sh ./tests/test1.exe tests/test1.in > tests/test1.out 2> tests/test1.err
 
-g++ tests/test2.cpp -pthread -o test2.exe -g
-./run.sh ./test2.exe tests/test2.in > tests/test2.out 2> tests/test2.err
+g++ tests/test2.cpp -pthread -o tests/test2.exe -g
+./run.sh ./tests/test2.exe tests/test2.in > tests/test2.out 2> tests/test2.err
