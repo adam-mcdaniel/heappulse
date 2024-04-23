085f13772c181d9af45ee5bf01e9cc63340be0e9
Adam McDaniel <adam.mcdaniel17@gmail.com>
2024-04-22 23:38:06 -0400

    Added compression to path

--- run.sh
+++ run.sh
@@ -12,7 +12,8 @@ fi
 shift
 
 # gdb setup:
-# gdb -ex "set environment LD_PRELOAD=$SCRIPT_DIR/libbkmalloc.so" -ex "set environment BKMALLOC_OPTS=--hooks-file=$SCRIPT_DIR/hook.so" -ex "run $PROGRAM_TO_RUN $@"
+# gdb -ex "set environment LD_PRELOAD=./libbkmalloc.so" -ex "set environment BKMALLOC_OPTS=--hooks-file=./hook.so"
+COMPRESSION_PATH="/usr/lib/x86_64-linux-gnu/liblz4.so:/usr/lib/x86_64-linux-gnu/liblzo2.so:/usr/lib/x86_64-linux-gnu/liblzf.so:/usr/lib/x86_64-linux-gnu/libsnappy.so:/usr/lib/x86_64-linux-gnu/libzstd.so"
 
 # LD_PRELOAD=$SCRIPT_DIR/libbkmalloc.so BKMALLOC_OPTS="--hooks-file=$SCRIPT_DIR/hook.so" "$PROGRAM_TO_RUN" "$@" 0<&-
-LD_PRELOAD=$SCRIPT_DIR/libbkmalloc.so BKMALLOC_OPTS="--hooks-file=$SCRIPT_DIR/hook.so --log-hooks" "$PROGRAM_TO_RUN" "$@" 0<&-
+LD_PRELOAD="$SCRIPT_DIR/libbkmalloc.so" BKMALLOC_OPTS="--hooks-file=\"$SCRIPT_DIR/hook.so\" --log-hooks" "$PROGRAM_TO_RUN" "$@" 0<&-
